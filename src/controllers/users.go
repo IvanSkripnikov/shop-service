75e3ef4c8aab9632ff398987ba5f4fa2a1942316
Ivan Skripnikov <greattramak@mail.ru>
2024-12-21 23:21:49 +0300

    Add methods to user controller

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -9,11 +9,80 @@ import (
 func GetUsersListV1(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-
 		helpers.GetUsersList(w, r)
-
 	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
 
+func GetUserV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		helpers.GetUser(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func AddLoyaltyV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		helpers.AddLoyalty(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func RemoveLoyaltyV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodDelete:
+		helpers.RemoveLoyalty(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func CreateUserV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		helpers.CreateUser(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func UpdateUserV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPut:
+		helpers.UpdateUser(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func BlockUserV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		helpers.BlockUser(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func ResetUserPasswordV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		helpers.ResetPassword(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func GetStatisticsV1(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		helpers.GetStatistics(w, r)
+	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
--- src/helpers/users.go
+++ src/helpers/users.go
@@ -10,3 +10,59 @@ func GetUsersList(w http.ResponseWriter, _ *http.Request) {
 	}
 	SendResponse(w, data, "/v1/users/list")
 }
+
+func GetUser(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"user": "OK",
+	}
+	SendResponse(w, data, "/v1/users/get")
+}
+
+func AddLoyalty(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"addLoyalty": "OK",
+	}
+	SendResponse(w, data, "/v1/users/add-loyalty")
+}
+
+func RemoveLoyalty(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"removeLoyalty": "OK",
+	}
+	SendResponse(w, data, "/v1/users/remove-loyalty")
+}
+
+func CreateUser(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"createUser": "OK",
+	}
+	SendResponse(w, data, "/v1/users/create")
+}
+
+func UpdateUser(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"updateUser": "OK",
+	}
+	SendResponse(w, data, "/v1/users/update")
+}
+
+func BlockUser(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"blockUser": "OK",
+	}
+	SendResponse(w, data, "/v1/users/block")
+}
+
+func ResetPassword(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"resetPassword": "OK",
+	}
+	SendResponse(w, data, "/v1/users/reset-password")
+}
+
+func GetStatistics(w http.ResponseWriter, _ *http.Request) {
+	data := ResponseData{
+		"getStatistics": "OK",
+	}
+	SendResponse(w, data, "/v1/users/statistics")
+}
