3a4534ccaaddced577fd56a52e29efca08242485
Ivan Skripnikov <greattramak@mail.ru>
2025-02-08 22:11:58 +0300

    Fix model and migration

--- src/migrations/01_create_users_table.sql
+++ src/migrations/01_create_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) DEFAULT '',
     created BIGINT UNSIGNED,
     updated BIGINT UNSIGNED,
     active TINYINT DEFAULT 1,
--- src/models/users.go
+++ src/models/users.go
@@ -3,6 +3,7 @@ package models
 type User struct {
 	ID        int    `json:"id"`
 	UserName  string `json:"username"`
+	Password  string `json:"password"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
