09378980c636fed370d091606bf348c01b41f009
Ivan Skripnikov <greattramak@mail.ru>
2025-02-08 17:02:40 +0300

    alter users table

--- src/migrations/01_create_users_table.sql
+++ src/migrations/01_create_users_table.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT auto_increment PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     created BIGINT UNSIGNED,
     updated BIGINT UNSIGNED,
-    active TINYINT DEFAULT 1
+    active TINYINT DEFAULT 1,
+    CONSTRAINT login_unique UNIQUE (username),
+    CONSTRAINT email_unigue UNIQUE (email)
 );
